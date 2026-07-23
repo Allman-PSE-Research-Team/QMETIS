6878b198d141c9970e94302470e0e058afed412c
Fortune Adekogbe <fortuneadekogbe@gmail.com>
2026-07-22 23:29:38 -0400

    Updated README to include explicit build instructions.

--- vsgen.bat
+++ vsgen.bat
@@ -1,7 +1,7 @@
 MKDIR build\windows
 MKDIR build\xinclude
-COPY include\metis.h build\xinclude
-COPY include\CMakeLists.txt build\xinclude
+COPY /Y include\metis.h build\xinclude
+COPY /Y include\CMakeLists.txt build\xinclude
 CD build\windows
 cmake -DCMAKE_CONFIGURATION_TYPES="Release" ..\.. %*
 ECHO VS files have been generated in build\windows
